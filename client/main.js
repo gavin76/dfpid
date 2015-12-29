fcbcdf77b8163df5c80d8e961e7b26a7cb8197b5
Gavin Chan <freshgav@gmail.com>
2015-12-29 23:30:31 +0800

    Create basic structure

---
+++ client/main.js
@@ -0,0 +1,59 @@
+// Session declaration
+Session.set("listingIngr", "");
+
+// Routing
+
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function() {
+  this.render('navbar', {
+    to: "navbar"
+  });
+  this.render('home', {
+    to: "main"
+  });
+});
+
+Router.route('/additem', function() {
+  this.render('navbar', {
+    to: "navbar"
+  });
+  this.render('add_item', function() {
+    to: "main"
+  });
+});
+
+Router.route('/about', function() {
+  this.render('navbar', {
+    to: "navbar"
+  });
+  this.render('about', function() {
+    to: "main"
+  });
+});
+
+// Accounts configuration
+
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_AND_EMAIL"
+});
+
+// Template helpers
+Template.product_list.helpers({
+  products: function() {
+    var findParam = Session.get("listingIngr");
+    console.log("List params: " + findParam);
+    return Products.find(findParam, {sort: {productName: -1}});
+  }
+});
+// Template events
+Template.ingr_search.events({
+  "submit .js-search-ingr-form": function(ev) {
+    ev.preventDefault();
+    var search_text = $('.js-search-ingr').val().toLowerCase();
+    console.log('Search for: ' + search_text);
+    Session.set('listingIngr', {"ingredients": search_text});
+  }
+});
--- server/startup.js
+++ server/startup.js
@@ -1,3 +1,75 @@
 Meteor.startup(function() {
-	
+
+	// Initialize database and example data if none found
+	if (!Products.findOne()) {
+		console.log("No products yet. Creating starter data.");
+
+		Products.insert({
+			category: "skin care",
+			subtype: "emollient",
+			manufacturer: "Galderma",
+			country: "Canada",
+			brandName: "Cetaphil",
+			productName: "Dailyadvance ultra hydrating lotion",
+			productId: "p51657-1",
+			source: "product label",
+			createdBy: "default",
+			createdOn: new Date(),
+			ingredients: [
+				"glycerin", "hydrogenated polysobutene", "cetearyl alcohol",
+				"butyrospermum parkii butter", "macadamia ternifolia", "seed oil",
+				"macadamia nut oil", "cyclopentasiloxane", "sodium PCA",
+				"tocopheryl acetate", "acrylates", "c10-30 alkyl acylate crosspolymer",
+				"benzyl alcohol", "ceteareth-20", "citric acid", "dimethiconol",
+				"famesol", "panthenol", "phenoxyethanol", "sodium hydroxide",
+				"sodium polyacrylate", "stearoxytrimethylsilane", "stearyl alcohol"
+			]
+		});
+		Products.insert({
+			category: "toiletry",
+			subtype: "shampoo",
+			manufacturer: "Some manufacturer",
+			country: "Singapore",
+			brandName: "Abrand",
+			productName: "Shampoo contains selenium sulphide and fragrance",
+			productId: "p51657asda-1",
+			source: "product label",
+			createdBy: "default",
+			createdOn: new Date(),
+			ingredients: [
+				"selenium sulphide", "fragrance"
+			]
+		});
+		Products.insert({
+			category: "household",
+			subtype: "disinfectant",
+			manufacturer: "Some manufacturer",
+			country: "Hong Kong",
+			brandName: "Anotherbrand",
+			productName: "Disinfectant product has bleach",
+			productId: "p51657asda-1",
+			source: "product label",
+			createdBy: "default",
+			createdOn: new Date(),
+			ingredients: [
+				"bleach", "fragrance"
+			]
+		});
+		Products.insert({
+			category: "household",
+			subtype: "detergent",
+			manufacturer: "Some manufacturer",
+			country: "Hong Kong",
+			brandName: "Anotherbrand",
+			productName: "Detergent",
+			productId: "p51657asda-1",
+			source: "product label",
+			createdBy: "default",
+			createdOn: new Date(),
+			ingredients: [
+				"benzyl alcohol", "fragrance"
+			]
+		});
+
+	}
 });
